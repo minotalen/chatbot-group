44ae28429ebf9c9d353e10230add7026f4434201
Martin Strotmann <strotman@uni-bremen.de>
2020-05-05 13:19:23 +0200

    folder structure and working client-server connection port 5000

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__, static_url_path='/static')
+
+app.config["SECRET_KEY"] = "Elefantengeheimnis"
+
+socketio = SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
+
+@app.route('/')
+def send_index_page():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def connect():
+    print("You are now connected with the server")
+
+@socketio.on('disconnect')
+def disconnect():
+    print("You are disconneced from the server")
+
+@socketio.on_error()
+def error_handler(e):
+    raise Exception("Some error happened, no further notice")
+
+if __name__ == "__main__" :
+    print("Try to start server...")
+    socketio.run(app, host='0.0.0.0',debug = True)
---
+++ static/js/client.js
@@ -0,0 +1,4 @@
+var socket = io.connect("http://127.0.0.1:5000");
+socket.on('connect', function () {
+    console.log("connected client");
+});

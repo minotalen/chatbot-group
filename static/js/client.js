f10e70623d083c83943be8f26b1e03f6cb7ebb95
Kevin Katzkowski <katzkows@uni-bremen.de>
2020-05-06 15:40:55 +0200

    client.js: receive messages event listener

--- static/js/client.js
+++ static/js/client.js
@@ -1,8 +1,23 @@
+/**
+ * Client-side script to receive, send and display messages.
+ * Authors: ?, Katja Schneider, Kevin Katzkowski
+ * Last modfidied: 06.05.2020
+ */
+
+
 var socket = io.connect("http://127.0.0.1:5000");
+
+
 socket.on('connect', function () {
     console.log("connected client");
 });
 
-	  socket.on('message', function(msg) {
-	  	  $(#"messages").append('<li>'+msg+#</li>');
-	  })
+
+
+/**
+ * Display received message from server in chat interface.
+ */
+socket.on('message', function(msg) {
+  let chat = document.getElementById('chat-content-container');
+  chat.append('<li>'+msg+'</li>');
+});

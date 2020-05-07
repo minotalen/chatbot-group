438ddf8d13795782791580e15aec1b91856dae21
Kevin Katzkowski <katzkows@uni-bremen.de>
2020-05-07 18:02:47 +0200

    client.js: JSON communication implemented

--- static/js/client.js
+++ static/js/client.js
@@ -8,7 +8,11 @@
 let socket = io.connect("http://127.0.0.1:5000"),
   sendButton = document.getElementById('send-button'),
   userInput = document.getElementById('input-user'),
-  chatForm = document.getElementById('chat-input-form');
+  chatForm = document.getElementById('chat-input-form'),
+  levelID = 'test_level_ID', 
+  senderName = 'test_sender_name',
+  roomName = 'test_room_name', 
+  itemList = [{item:'test_item_name1', action:'test_item_action1'},{item: 'test_item_name2', action: 'test_item_action2'}];
 
 
 socket.on('connect', function () {
@@ -18,8 +22,10 @@ socket.on('connect', function () {
 /**
  * Display received message from socket in chat interface.
  */
-socket.on('message', (msg) => {
+socket.on('json', (json) => {
   console.log('message received');
+  msg = readJSON(json);
+
   printMessage(msg, 'bot');
 });
 
@@ -37,11 +43,12 @@ chatForm.addEventListener('submit', (evt) => {
  * Sends the message from the chat input to the socket.
  */
 function sendMessage() {
-  let msg = userInput.value;
+  let json, msg = userInput.value;
 
-  // send message to socket
-  if(msg != '') {
-    socket.send(msg);
+  // send JSON String to socket
+  if(msg.trim() != '') {
+    json = createJSON(msg);
+    socket.send(json);
     userInput.value = null;
     console.log('message ' + msg + ' has been sent!');
     
@@ -62,9 +69,9 @@ function printMessage(msg, msgType) {
     elem = window.document.createElement('li');
 
   elem.innerHTML = msg;
-
+  
   // set message type
-  switch (msgType) {
+  switch (senderName) {
     case 'bot':
       elem.className = 'chat-message-bot';
       break;
@@ -78,4 +85,55 @@ function printMessage(msg, msgType) {
 
   // scroll to bottom
   chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+}
+
+
+/**
+ * Creates a new JSON String using the data from client variables and the specified message.
+ * @param {String} msg the message to be transported in the JSON String
+ * @returns {JSON String} the generated JSON String 
+ */
+function createJSON(msg) {
+  let obj, json;
+
+  // create object to parse into JSON
+  obj = {
+    level: levelID,
+    sender: senderName,
+    room: roomName,
+    items: itemList,
+    message: msg
+  };
+
+  // parse object into JSON String
+  json = JSON.stringify(obj);
+  console.log(json);
+
+  // TODO remove
+  readJSON(json);
+
+  return json;
+}
+
+
+/**
+ * Reads a JSON String, stores its data in client variables and returns the message.
+ * @param {JSON String} json the JSON String
+ * @returns {String} the message transported in the JSON String 
+ */
+function readJSON(json) {
+  // parse JSON String into JS object
+  let message, obj = JSON.parse(json);
+  console.log(obj);
+
+  // update client variables
+  levelID = obj.level;
+  senderName = obj.sender;
+  roomName = obj.room;
+  itemList = obj.items;
+  message = obj.message;
+
+  console.log(message);
+  
+  return message;
 }

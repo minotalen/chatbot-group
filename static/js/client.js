2d0e96af3d7358222e07d3c58cf627f9b3a5f69e
katjajs <49094807+katjajs@users.noreply.github.com>
2020-05-08 11:54:37 +0200

    send username

--- static/js/client.js
+++ static/js/client.js
@@ -29,6 +29,12 @@ socket.on('json', (json) => {
   printMessage(msg);
 });
 
+var private_socket =io('http://127.0.0.1:5000/private')
+
+$('#send_username').on('click', function() {
+  private_socket.emit('username', $('#username').val());
+}
+)
 
 sendButton.addEventListener('click', sendMessage, false);
 

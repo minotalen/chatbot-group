9f2dee0d5ff216274f277b360a676b3286f4fdba
Martin Strotmann <strotman@uni-bremen.de>
2020-05-08 12:02:48 +0200

    fixed override

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send
+import json
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -20,12 +21,10 @@ def handleJson(payload):
     send(payload,json=True)
 
 
-# this can be removed since it has been replaced by JSON
-@socketio.on('message')
-def handleMessage(msg): 
-    print(request.sid)
-    print('Message: ' + msg)
-    send(msg, broadcast=True)
+@socketio.on('user_registration')
+def update_users(payload):
+    users.append({"user_id" : request.sid, "user_name" : payload['message']})
+    print("added user: " + payload['message'] + "with session id: " + request.sid)
 
 @socketio.on('username', namespace='/private')
 def receive_username(username):
@@ -34,6 +33,9 @@ def receive_username(username):
 
 @socketio.on('connect')
 def connect():
+    initial_data = {"level": 1,"sender": "server","room":"First Hallway","items":[],"message": "Welcome!"}
+    json_data = json.dumps(initial_data)
+    send(json_data, json=True)
     print("You are now connected with the server")
 
 @socketio.on('disconnect')

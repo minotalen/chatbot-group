ce187123ea1dbe170246076677082a2011200484
katjajs <schnekat@uni-bremen.de>
2020-05-11 18:37:45 +0200

    Revert "message function"
    This reverts commit 62d26a4f2423d806713857d181d3fc2db1f5dccc.

--- static/js/client.js
+++ static/js/client.js
@@ -2,7 +2,3 @@ var socket = io.connect("http://127.0.0.1:5000");
 socket.on('connect', function () {
     console.log("connected client");
 });
-
-	  socket.on('message', function(msg) {
-	  	  $(#"messages").append('<li>'+msg+#</li>');
-	  })

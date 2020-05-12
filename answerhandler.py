89ac9d08a730301691ce7bf2f16181c10945b63d
mape <mape@uni-bremen.de>
2020-05-12 11:41:09 +0200

    reversed

--- answerhandler.py
+++ answerhandler.py
@@ -49,7 +49,7 @@ def findEntry(id, column):
     file_location = script_location / 'roomsGW2.csv'
     file = file_location.open()
     
-    with open(file_location, 'r',  newline = '', encoding = 'utf-8') as file:
+    with open(file_location, 'r') as file:
         reader = csv.reader(file, delimiter='§')
         rooms = [row for row in reader]
 
@@ -71,7 +71,7 @@ def getRoomId(msg):
     file_location = script_location / 'roomsGW2.csv'
     file = file_location.open()
     
-    with open(file_location, 'r',  newline = '', encoding = 'utf-8') as file:
+    with open(file_location, 'r') as file:
         reader = csv.reader(file, delimiter='§' )
         rooms = [row for row in reader]
         

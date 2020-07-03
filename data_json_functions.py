d376d537d354deebfeedb4e0a840a402177d48ce
Kristin Wünderlich <krissilino@web.de>
2020-07-03 15:03:46 +0200

    Fix out of range bug bei actions 147

--- data_json_functions.py
+++ data_json_functions.py
@@ -70,29 +70,29 @@ def doAction(actionName, actionParam, roomId, username):
     #Action not recognized
     if actionId == -1:
         print("This action doesn't exist: "+actionName)
-        return (None,None)
+        return (None,None, None)
 
     #Change the mode
     elif actionId == 1:
         print("Change the mode to "+actionParam)
-        return (None, actionParam)
+        return (None, actionParam, None)
     
     #Add an item    
     elif actionId == 2:
         print("This item is added: "+actionParam)
         add_to_inventory(actionParam, roomId, username)
-        return (None,None)
+        return (None,None, None)
     
     #Remove an item    
     elif actionId == 3:
         print("This item is deleted: "+actionParam)
         remove_from_inventory(actionParam, roomId, username)
-        return (None,None)
+        return (None,None, None)
 
     #Changes the room of the player
     elif actionId == 4:
         print("The player moves to room no.: "+ str(actionParam))
-        return(actionParam, None)
+        return(actionParam, None, None)
     
     #Changes the sender of the message
     elif actionId == 5:

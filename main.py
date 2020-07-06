22f183f9adef55a1f96dc642e930258b2d611c3d
MartinGitmann <64006844+MartinGitmann@users.noreply.github.com>
2020-07-06 15:38:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -166,4 +166,4 @@ def get_current_username():
 
 if __name__ == "__main__":
     print("Trying to start server...")
-    socketio.run(app, host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)

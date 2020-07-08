109bf4c115d6e32e0acc734464b32a1c408425d5
MartinGitmann <64006844+MartinGitmann@users.noreply.github.com>
2020-07-08 15:51:43 +0200

    Add files via upload

--- audio.py
+++ audio.py
@@ -1,9 +1,9 @@
-#import os
-#import datetime
-#from gtts import gTTS
-#from playsound import playsound
+import os
+import datetime
+from gtts import gTTS
+from playsound import playsound
  
-#i = 0
+i = 0
 
 """
 @author Max Petendra
@@ -14,12 +14,12 @@ Parameters:
 inputtext: The inputtext that sould be converteted to a mp3
 audio filename: The audio outplut filename default is given.
 """
-#def playSound(inputtext: str = ""):
-#    global i
-#    text2speach = gTTS(text = inputtext , lang = 'en')
-#    filename = str(i) + ".mp3"
-#    i = i + 1 
-#    text2speach.save(filename) 
-#    playsound(filename)
-#    os.remove(filename)
+def playSound(inputtext: str = ""):
+    global i
+    text2speach = gTTS(text = inputtext , lang = 'en')
+    filename = str(i) + ".mp3"
+    i = i + 1 
+    text2speach.save(filename) 
+    playsound(filename)
+    os.remove(filename)
 

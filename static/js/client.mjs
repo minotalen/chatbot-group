ae201620bf2c73e158a0ab2abdc65a86b56a74fd
MartinGitmann <64006844+MartinGitmann@users.noreply.github.com>
2020-07-08 15:31:33 +0200

    Update client.mjs

--- static/js/client.mjs
+++ static/js/client.mjs
@@ -10,7 +10,7 @@ import {
 } from './suggestions.mjs';
 
 
-let socket = io.connect("http://127.0.0.1:5000"),
+let socket = io.connect('https://' + document.domain + ':' + location.port),
   sendButton = document.getElementById('send-button'),
   userName = undefined,
   levelID = 'test_level_ID',
